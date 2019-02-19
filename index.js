deace965b849aa4eb746460457e1b5f96e95acc2
sabud1208 <43481963+sabud1208@users.noreply.github.com>
2019-02-19 16:46:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+
+newHeader.innerText = "YOUR-NAME is the champion";
+
+
+document.body.append(newHeader)
